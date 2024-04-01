fd89715c360dc4e7a133cae231b92173990d6d92
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2024-04-01 21:44:58 +0200

    useFrame

--- R3F/src/Experience.jsx
+++ R3F/src/Experience.jsx
@@ -1,13 +1,29 @@
 import { MeshBasicMaterial } from "three";
+import { useFrame } from "@react-three/fiber";
 
 export default function Experience() {
+  
+    useFrame(() =>
+    {
+        
+    })
+    
   return (
     <>
-      <mesh rotateY={Math.PI * 0.25} position-x={2} scale={1.5}>
-       <boxGeometry/>
-       
-        <meshBasicMaterial color="orange" wireframe />
-      </mesh>
+    <mesh position-x={ - 2 }>
+    <sphereGeometry />
+    <meshBasicMaterial color="orange" wireframe/>
+</mesh>
+
+<mesh rotation-y={ Math.PI * 0.25 } position-x={ 2 } scale={ 1.5 }>
+    <boxGeometry/>
+    <meshBasicMaterial color="mediumpurple" wireframe/>
+</mesh>
+
+<mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+    <planeGeometry/>
+    <meshBasicMaterial  color="greenyellow" />
+</mesh>
     </>
   );
 }
